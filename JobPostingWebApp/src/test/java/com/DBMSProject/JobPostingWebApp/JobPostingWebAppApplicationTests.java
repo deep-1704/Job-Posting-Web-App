0f384957b825247197b122ec53db501c11cb0c5c
kravi770 <770rkravi@gmail.com>
2024-03-25 15:32:41 +0530

    Backend: Initialize Spring Boot application and add schema.sql for defining database structure of JobPostingWebApp

---
+++ JobPostingWebApp/src/test/java/com/DBMSProject/JobPostingWebApp/JobPostingWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DBMSProject.JobPostingWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPostingWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
