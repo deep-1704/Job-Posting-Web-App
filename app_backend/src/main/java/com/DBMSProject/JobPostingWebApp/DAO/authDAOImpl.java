b5992b58a8fd0705fcf5121d6ae39daf9e9726b0
raghava7129 <21cs01068@iitbbs.ac.in>
2024-04-07 10:48:46 +0530

    Database: Implement saveUser function in authDAOImpl file.

--- app_backend/src/main/java/com/DBMSProject/JobPostingWebApp/DAO/authDAOImpl.java
+++ app_backend/src/main/java/com/DBMSProject/JobPostingWebApp/DAO/authDAOImpl.java
@@ -21,7 +21,7 @@ public class authDAOImpl implements authDAO {
 
     @Override
     public users getUser(String username) {
-        try{
+        try {
             Connection con = DriverManager.getConnection(url, user, password);
             System.out.println("connected to db!!");
 
@@ -44,7 +44,7 @@ public class authDAOImpl implements authDAO {
                 String gender = rs.getString("gender");
 
                 Boolean enabledBool = false;
-                if(enabled.equals("T")){
+                if (enabled.equals("T")) {
                     enabledBool = true;
                 }
 
@@ -54,24 +54,58 @@ public class authDAOImpl implements authDAO {
 
             con.close();
             rs.close();
-        }catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
     }
 
-
-    // Testing the getUser method
-//    public static void main(String[] args){
-//        authDAOImpl auth = new authDAOImpl();
-//        users user = auth.getUser("Raghava");
-//        System.out.println(user);
-//    }
-
     @Override
-    public users saveUser(users user) {
+    public users saveUser(users User) {
+        try {
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("connected to db!!");
+
+            String query =
+                    "insert into users(username, password, FULL_NAME, EMAIL, USER_ROLE, ENABLED, PHONE, GENDER) " +
+                            "values(?, ?, ?, ?, ?, ?, ?, ?)";
+
+            Boolean isEnabled = User.isEnabled();
+            String enabled = "F";
+            if (isEnabled) {
+                enabled = "T";
+            }
+
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setString(1, User.getUsername());
+            pstmt.setString(2, User.getPassword());
+            pstmt.setString(3, User.getFull_name());
+            pstmt.setString(4, User.getEmail());
+            pstmt.setString(5, User.getUser_role());
+            pstmt.setString(6, enabled);
+            pstmt.setString(7, User.getPhone());
+            pstmt.setString(8, User.getGender());
+
+            pstmt.executeUpdate();
+
+            return User;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return null;
+
     }
 
+    //         Testing the saveUser method
+//    public static void main(String[] args) {
+//        authDAOImpl authDAO = new authDAOImpl();
+//        users user = authDAO.saveUser(new users("test", "test", "test",
+//                "test@gmail.com", "job_seeker",
+//                true, "7998473248", "Male"));
+//
+//        System.out.println(user);
+//
+//    }
 
 }
