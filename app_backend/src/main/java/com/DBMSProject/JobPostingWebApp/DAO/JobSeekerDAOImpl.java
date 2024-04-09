18a2af92c38a0ae073dbac244cfd327fae133e53
raghava7129 <21cs01068@iitbbs.ac.in>
2024-04-09 15:50:41 +0530

    Database: Implement getJobSeekerProfile function in JobSeekerDAOImpl file

--- app_backend/src/main/java/com/DBMSProject/JobPostingWebApp/DAO/JobSeekerDAOImpl.java
+++ app_backend/src/main/java/com/DBMSProject/JobPostingWebApp/DAO/JobSeekerDAOImpl.java
@@ -7,6 +7,9 @@ import org.springframework.stereotype.Component;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class JobSeekerDAOImpl implements JobSeekerDAO{
@@ -81,6 +84,115 @@ public class JobSeekerDAOImpl implements JobSeekerDAO{
 
     @Override
     public getJobSeekerResponse getJobSeekerProfile(String username) {
-        return null;
+        getJobSeekerResponse JobSeekerResponseObj = null;
+
+        try {
+            String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+            String user = "c##jobpostingwebapp";
+            String password = "jobpostingwebapp";
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("connected to db!!");
+
+            String brief_description = null;
+            String resume_link = null;
+            String job_type_preference = null;
+            String expected_salary = null;
+            String year_of_graduation = null;
+            String degree = null;
+            String major = null;
+
+            String query1 = "select BRIEF_DESCRIPTION, RESUME_LINK, JOB_TYPE_PREFERENCE, EXPECTED_SALARY," +
+                    "YEAR_OF_GRADUATION, DEGREE, MAJOR from JOB_SEEKERS where username = ?";
+
+            PreparedStatement pstmt1 = con.prepareStatement(query1);
+            pstmt1.setString(1, username);
+
+            ResultSet rs1 = pstmt1.executeQuery();
+
+
+
+            int rs1_size = 0;
+            while(rs1.next()){
+                rs1_size++;
+                if(rs1_size > 1){
+                    return null;
+                }
+
+                brief_description = rs1.getString("BRIEF_DESCRIPTION");
+                resume_link = rs1.getString("RESUME_LINK");
+                job_type_preference = rs1.getString("JOB_TYPE_PREFERENCE");
+                expected_salary = rs1.getString("EXPECTED_SALARY");
+                year_of_graduation = rs1.getString("YEAR_OF_GRADUATION");
+                degree = rs1.getString("DEGREE");
+                major = rs1.getString("MAJOR");
+            }
+
+            String query2 = "select full_name, email, phone, gender from users where username = ?";
+            PreparedStatement pstmt2 = con.prepareStatement(query2);
+            pstmt2.setString(1, username);
+
+            ResultSet rs2 = pstmt2.executeQuery();
+
+            int rs2_size = 0;
+
+            String full_name = null;
+            String email = null;
+            String phone = null;
+            String gender = null;
+
+
+            while(rs2.next()){
+                rs2_size++;
+                if(rs2_size > 1){
+                    return null;
+                }
+
+
+                full_name = rs2.getString("full_name");
+                email = rs2.getString("email");
+                phone = rs2.getString("phone");
+                gender = rs2.getString("gender");
+            }
+
+            String query3 = "select skill from user_skills where username = ?";
+            List<String> skills_list = new ArrayList<>();
+
+            PreparedStatement pstmt3 = con.prepareStatement(query3);
+            pstmt3.setString(1, username);
+            ResultSet rs3 = pstmt3.executeQuery();
+
+            while(rs3.next()){
+                skills_list.add(rs3.getString("skill"));
+            }
+
+            String[] skills = new String[skills_list.size()];
+
+            for(int i=0;i<skills_list.size();i++){
+                skills[i] = skills_list.get(i);
+            }
+
+
+            if(rs1_size == 1 && rs2_size == 1){
+
+
+
+
+
+                JobSeekerResponseObj = new getJobSeekerResponse(username, full_name, email,
+                        phone, gender, brief_description, skills, resume_link,
+                        "job_seeker", job_type_preference, expected_salary, year_of_graduation,
+                        degree, major);
+
+                return JobSeekerResponseObj;
+            }
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return JobSeekerResponseObj;
+
     }
 }
