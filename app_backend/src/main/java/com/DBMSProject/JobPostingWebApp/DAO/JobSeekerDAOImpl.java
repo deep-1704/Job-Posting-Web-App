aaabd8a21deabad488adfe38b7c6605b6145cb53
raghava7129 <21cs01068@iitbbs.ac.in>
2024-04-11 02:09:04 +0530

    Database: Implement getJobSeekerJobs function in JobSeekerDAOImpl file

--- app_backend/src/main/java/com/DBMSProject/JobPostingWebApp/DAO/JobSeekerDAOImpl.java
+++ app_backend/src/main/java/com/DBMSProject/JobPostingWebApp/DAO/JobSeekerDAOImpl.java
@@ -9,8 +9,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class JobSeekerDAOImpl implements JobSeekerDAO{
@@ -176,10 +179,6 @@ public class JobSeekerDAOImpl implements JobSeekerDAO{
 
             if(rs1_size == 1 && rs2_size == 1){
 
-
-
-
-
                 JobSeekerResponseObj = new getJobSeekerResponse(username, full_name, email,
                         phone, gender, brief_description, skills, resume_link,
                         "job_seeker", job_type_preference, expected_salary, year_of_graduation,
@@ -199,6 +198,99 @@ public class JobSeekerDAOImpl implements JobSeekerDAO{
 
     @Override
     public List<getJobSeekerJobsResponse> getJobSeekerJobs(String username) {
-        return List.of();
+
+        List<getJobSeekerJobsResponse> jobSeekerJobsList = new ArrayList<>();
+
+        try {
+            String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+            String user = "c##jobpostingwebapp";
+            String password = "jobpostingwebapp";
+
+            Connection con = DriverManager.getConnection(url, user, password);
+            System.out.println("connected to db!!");
+
+            int job_id = 0;
+            String job_title = null;
+            String job_location = null;
+            String job_description = null;
+            String job_salary = null;
+            String job_type = null;
+            String job_deadline = null;
+            String company_name = null;
+            String relevant_company_link = null;
+
+            String query = "select distinct j.job_id , j.job_title, jl.LOCATION, j.JOB_DESCRIPTION, j.JOB_SALARY, j.JOB_TYPE, j.JOB_DEADLINE,jp.COMPANY_NAME, j.RELEVANT_COMPANY_LINK\n" +
+                    "from jobs j join JOB_LOCATIONS jl on jl.JOB_ID = j.job_id " +
+                    "join JOB_POSTERS jp on jp.USERNAME = j.job_poster " +
+                    "JOIN job_skills js ON js.job_id = j.job_id " +
+                    "JOIN user_skills us ON UPPER(js.SKILL) = UPPER(us.SKILL) " +
+                    "WHERE us.USERNAME = ?" +
+                    "and not exists (select ja.username from job_applications ja where ja.job_id = j.job_id and ja.username = ?) " +
+                    "ORDER BY j.job_id  ";
+
+            PreparedStatement pstmt1 = con.prepareStatement(query);
+            pstmt1.setString(1, username);
+            pstmt1.setString(2, username);
+
+            ResultSet rs1 = pstmt1.executeQuery();
+
+            Map<Integer, getJobSeekerJobsResponse> jobSeekerObjMap = new HashMap<>();
+            Map<Integer, List<String>> locationMap = new HashMap<>();
+
+            while(rs1.next()) {
+                job_id = rs1.getInt(1);
+                job_title = rs1.getString(2);
+                job_location = rs1.getString(3);
+                job_description = rs1.getString(4);
+                job_salary = rs1.getString(5);
+                job_type = rs1.getString(6);
+                job_deadline = rs1.getString(7);
+                company_name = rs1.getString(8);
+                relevant_company_link = rs1.getString(9);
+
+                getJobSeekerJobsResponse jobSeekerJobsResponseObj = new getJobSeekerJobsResponse();
+
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                jobSeekerJobsResponseObj.setJob_id(job_id);
+                jobSeekerJobsResponseObj.setJob_title(job_title);
+                jobSeekerJobsResponseObj.setJob_salary(Integer.parseInt(job_salary));
+                jobSeekerJobsResponseObj.setJob_type(job_type);
+                jobSeekerJobsResponseObj.setDeadline(sdf.parse(job_deadline));
+
+                List<String> company_list = new ArrayList<>();
+                company_list.add(company_name);
+                company_list.add(relevant_company_link);
+
+                jobSeekerJobsResponseObj.setCompany(company_list);
+
+                jobSeekerObjMap.put(job_id, jobSeekerJobsResponseObj);
+
+                if(locationMap.containsKey(job_id)) {
+                    locationMap.get(job_id).add(job_location);
+                }else{
+                    List<String> locationList = new ArrayList<>();
+                    locationList.add(job_location);
+                    locationMap.put(job_id, locationList);
+                }
+
+            }
+
+
+            for(getJobSeekerJobsResponse jobSeekerJobsResponseObj : jobSeekerObjMap.values()){
+                int job_id_temp = jobSeekerJobsResponseObj.getJob_id();
+                jobSeekerJobsResponseObj.setJob_location(locationMap.get(job_id_temp));
+
+                jobSeekerJobsList.add(jobSeekerJobsResponseObj);
+            }
+
+
+            return jobSeekerJobsList;
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return jobSeekerJobsList;
     }
 }
